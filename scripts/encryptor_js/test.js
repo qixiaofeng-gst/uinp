fce26184b1ef1f2c5ecd2625df989b50dee19bea
XiaofengQi <qixiaofeng@gsegment.com>
2018-07-27 10:49:51 +0800

    Tiny function added.

--- scripts/encryptor_js/test.js
+++ scripts/encryptor_js/test.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
-console.log('Damn the whole thing. The process.stdin stream not work as expected on cygwin.')
+console.log('The process.stdin stream not work as expected on cygwin.')
+
+const fs = require('fs')
+const { execSync: es } = require('child_process')
+
+const files = fs.readdirSync('.')
+const space = ' '
+for (const f of files) {
+	const idx = f.indexOf(space)
+	if (idx >= 0) {
+		const updated = f.substr(0, idx) + f.substr(idx + 1, 10)
+		console.log(f, '>', updated)
+		es(`mv '${f}' ${updated}`)
+	}
+}
