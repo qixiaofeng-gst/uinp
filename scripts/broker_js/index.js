7f272754a10636745d63bc7d5bc722c1a65b0776
XiaofengQi <qixiaofeng@gsegment.com>
2019-06-26 09:12:41 +0800

    Added module.exports things to broker_js

--- scripts/broker_js/index.js
+++ scripts/broker_js/index.js
@@ -11,53 +11,35 @@ const read_into = (file_path, context) => {
     modify_time:  fs.statSync(file_path).mtimeMs,
   }
 }
-const parsers = [
-  /\/\*PUT\s+(.+)\s+\*\//,
-  /<!--PUT\s+(.+)\s+-->/,
-]
+const parsers = {
+  clike_put: {
+    p: /\/\*PUT\s+(.+)\s+\*\//,
+    cb_replacement: false,
+  },
+  html_put: {
+    p: /<!--PUT\s+(.+)\s+-->/,
+    cb_replacement: false,
+  },
+  require: {
+    p: /require\('(.+)'\)/,
+    cb_replacement: file_path => `modules['${(() => file_path)()}']`,
+  },
+}
 
 const self = Symbol('self')
-const merge = Symbol('merge')
-const Assignable = () => {
-  const mami = {}
-  
-  const absorb = val => {
-    for (const k in val) {
-      const v = val[k]
-      if (undefined == val) {
-        continue
-      }
-      mami[k] = v
-    }
-    return true
-  }
-  
-  const clear = () => {
-    for (const k in mami) {
-      delete mami[k]
-    }
-  }
-  
-  return new Proxy(mami, {
-    get: (_, key, __) => mami[key],
-    set: (_, key, val, __) => {
-      if (key === self) {
-        clear()
-        return absorb(val)
-      }
-      
-      if (key === merge) {
-        return absorb(val)
-      }
-      
-      mami[key] = val
-      return true
-    },
-  })
-}
 const rejoin_path = target => target.replace(/\\/g, path.sep)
 
-const src_state = Assignable()
+const src_state = {}
+const modules = {}
+
+//const inner_sm = 
+const serialize_modules = () => `const modules = {\n${(() => {
+  let result = ''
+  for (const key in modules) {
+    result += `'${key}': (() => {\n${modules[key]}\n})(),\n`
+  }
+  return result
+})()}}//End modules\n`
 
 const start_watch = config => {
   const { input } = config
@@ -100,11 +82,24 @@ const start_watch = config => {
   })
 }
 
-const combine = (in_file_path, context) => {
+const combine = (in_file_path, context, mds) => {
   const loop_detector = []
   const ref_count = {}
   
   console.log('Combine with file:', in_file_path)
+  const de_modulize = content => {
+    const exp = content.match(/module\.exports\s*=\s*/gm)
+    if (null === exp) {
+      return
+    }
+    if (1 < exp.length) {
+      console.warn(`Current module has more than one 'module.exports =' statement, output might be invalid`)
+      return
+    }
+    content = content.replace(exp[0], 'return (')
+    content = content + ')'
+    return content
+  }
   
   const inner_parse = (file_path, file_content) => {
     if (loop_detector.includes(file_path)) {
@@ -125,7 +120,8 @@ const combine = (in_file_path, context) => {
     loop_detector.push(file_path)
     const src_dir = path.dirname(file_path)
     let c = file_content
-    for (const p of parsers) {
+    for (const key in parsers) {
+      const { p, cb_replacement } = parsers[key]
       const puts = c.match(new RegExp(p, 'gm'))
       if (null == puts) {
         continue
@@ -139,7 +135,14 @@ const combine = (in_file_path, context) => {
         if (false === context.hasOwnProperty(inner_file_path)) {
           read_into(inner_file_path, context)
         }
-        c = c.replace(put, inner_parse(inner_file_path, context[inner_file_path].content))
+        if (cb_replacement) {
+          if (false === mds.hasOwnProperty(inner_file_path)) {
+            mds[inner_file_path] = de_modulize(inner_parse(inner_file_path, context[inner_file_path].content))
+          }
+          c = c.replace(put, cb_replacement(inner_file_path))
+        } else {
+          c = c.replace(put, inner_parse(inner_file_path, context[inner_file_path].content))
+        }
       }
     }
     loop_detector.pop()
@@ -169,7 +172,8 @@ const combine = (in_file_path, context) => {
 const make_output = config => {
   const { input, output } = config
   const out_file = path.join(output, path.basename(input))
-  fs.writeFileSync(out_file, combine(input, src_state))
+  const combined = combine(input, src_state, modules)
+  fs.writeFileSync(out_file, serialize_modules() + combined)
   console.log('Combined to', out_file)
 }
 
