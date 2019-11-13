618006f3dae4165d686487ab28467158030b9b08
XiaofengQi <qixiaofeng@gsegment.com>
2019-11-13 12:10:40 +0800

    st

--- scripts/secs/correlation.js
+++ scripts/secs/correlation.js
@@ -36,8 +36,103 @@
     return (sum / length).toFixed(4)
   }
 
+  const
+    xmlns = 'http://www.w3.org/2000/svg',
+    msg_never_touch_me = 'Never touch me',
+    never_touch_me = new Proxy({}, {
+      get(_target, _p, _receiver) {
+        throw msg_never_touch_me
+      },
+      set(_target, _p, _value, _receiver) {
+        return false
+      },
+    }),
+    add = Symbol('add'),
+    cut = Symbol('cut'),
+    new_svg = tag => {
+      const
+        view = document.createElementNS(xmlns, tag),
+        svg_prefix = 'svg_',
+        css_prefix = 'css_',
+        view_key = 'view',
+        accessors = {
+          get: (_target, p, _receiver) => {
+            if (view_key === p) {
+              return view
+            } else if (add === p) {
+              return element => view.appendChild(element)
+            } else if (cut === p) {
+              return element => view.removeChild(element)
+            } else {
+              console.error(`${p} is touched unexpectedly.`)
+              throw msg_never_touch_me
+            }
+          },
+          set: (_target, p, value, _receiver) => {
+            if (p.startsWith(svg_prefix)) {
+              view.setAttributeNS(null, p.substr(4), value)
+            } else if (p.startsWith(css_prefix)) {
+              view.style[p.substr(4)] = value
+            } else {
+              view[p] = value
+            }
+            return true
+          },
+        }
+
+      return new Proxy(never_touch_me, accessors)
+    }
+
+  class SC {
+    #svgProxy = new_svg('svg')
+
+    constructor() {
+      this.#svgProxy.css_width = '300px'
+      this.#svgProxy.css_height = '300px'
+      this.#svgProxy.svg_viewBox = '0 0 100 100'
+      document.body.appendChild(this.#svgProxy.view)
+    }
+
+    addChild(elementProxy) {
+      this.#svgProxy[add](elementProxy.view)
+      return this
+    }
+
+    cutChild(elementProxy) {
+      this.#svgProxy[cut](elementProxy.view)
+      return this
+    }
+
+    newPoint(x, y) {
+      const circle = new_svg('circle')
+      circle.svg_cx = x
+      circle.svg_cy = y
+      circle.svg_r = 1
+      return circle
+    }
+
+    newLine(pairs) {
+      const line = new_svg('polyline')
+      line.svg_points = pairs.map(([x, y]) => `${x},${y}`).join(' ')
+      line.svg_stroke = 'black'
+      line.svg_fill = 'white'
+      return line
+    }
+  }
+
   console.log(cr([[1, .2], [2, .3], [3, .6], [4, .8], [6, 30]]), '==== [[1, .2], [2, .3], [3, .6], [4, .8], [6, 30]]')
   console.log(cr([[1, .2], [1, .3], [1, .6], [1, .8], [1, 30]]), '==== [[1, .2], [1, .3], [1, .6], [1, .8], [1, 30]]')
   console.log(cr([[1, .1], [1, .1], [1, .1], [1, .1], [1, .1]]), '==== [[1, .1], [1, .1], [1, .1], [1, .1], [1, .1]]')
   console.log(cr([[1, 6], [2, 6], [3, 6], [4, 6], [5, 6]]), '==== [[1, 6], [2, 6], [3, 6], [4, 6], [5, 6]]')
+
+  window.onload = () => {
+    const
+      svgCanvas = new SC(),
+      firstPoint = svgCanvas.newPoint(20, 20)
+    svgCanvas
+      .addChild(svgCanvas.newPoint(50, 50))
+      .addChild(firstPoint)
+      .cutChild(firstPoint)
+      .addChild(svgCanvas.newLine([[1, 10], [3, 4], [5, 6], [7, 8], [9, 10]]))
+  }
 })()
