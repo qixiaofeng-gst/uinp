cca11104b175b9ea0fbaea6891f415fd69b0bd20
qi_xiaofeng <qi_xiaofeng@foxmail.com>
2020-04-15 09:31:07 +0800

    Write table to file.

--- gists/learn-gnu-make/sandbox.c
+++ gists/learn-gnu-make/sandbox.c
@@ -1,11 +1,25 @@
+#include <stdio.h>
 #include <wchar.h>
 #include <locale.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
-int main(int argc, char const *argv[]) {
-    wprintf(L"%s\n", setlocale(LC_ALL, ""));
-    wprintf(L">>>>>>> Belows are sandbox output:\n");
+void
+showUnicodeTable()
+{
+    int count = 0;
+    for (int i = 0x2400; i < 0x2700; ++i) {
+        ++count;
+        wprintf(L"%lc, %X; ", i, i);
+        if ( 0 == ( count % 20 ) ) {
+            wprintf(L"\n");
+        }
+    }
+}
 
+void
+generateTableFile()
+{
     #define N L'\n', /* New line. */
     #define x L'\x253C', /* Cross line single-ed. */
     #define H L'\x2550', /* Horizontal line double-ed. */
@@ -20,15 +34,39 @@ int main(int argc, char const *argv[]) {
     #define m L'\x2562', /* Middle-right double-ed. */
     #define C L'\x2564', /* Center-top double-ed. */
     #define c L'\x2567', /* Center-bottom double-ed. */
-    #define O L'O', /* First-hand piece. */
-    #define o L'X', /* Second. */
     #define S L' ', /* Second. */
     const wchar_t table[] = {
-        L H H H C H H R N
-        V O o S v S S V N
-        M h h h x h h m N
-        V S S S v S S V N
-        l H H H c H H r N
+        L H C H C H C H C H C H C H C H C H C H C H C H C H C H C H R N
+        V S v S v S v S v S v S v S v S v S v S v S v S v S v S v S V N
+        M h x h x h x h x h x h x h x h x h x h x h x h x h x h x h m N
+        V S v S v S v S v S v S v S v S v S v S v S v S v S v S v S V N
+        M h x h x h x h x h x h x h x h x h x h x h x h x h x h x h m N
+        V S v S v S v S v S v S v S v S v S v S v S v S v S v S v S V N
+        M h x h x h x h x h x h x h x h x h x h x h x h x h x h x h m N
+        V S v S v S v S v S v S v S v S v S v S v S v S v S v S v S V N
+        M h x h x h x h x h x h x h x h x h x h x h x h x h x h x h m N
+        V S v S v S v S v S v S v S v S v S v S v S v S v S v S v S V N
+        M h x h x h x h x h x h x h x h x h x h x h x h x h x h x h m N
+        V S v S v S v S v S v S v S v S v S v S v S v S v S v S v S V N
+        M h x h x h x h x h x h x h x h x h x h x h x h x h x h x h m N
+        V S v S v S v S v S v S v S v S v S v S v S v S v S v S v S V N
+        M h x h x h x h x h x h x h x h x h x h x h x h x h x h x h m N
+        V S v S v S v S v S v S v S v S v S v S v S v S v S v S v S V N
+        M h x h x h x h x h x h x h x h x h x h x h x h x h x h x h m N
+        V S v S v S v S v S v S v S v S v S v S v S v S v S v S v S V N
+        M h x h x h x h x h x h x h x h x h x h x h x h x h x h x h m N
+        V S v S v S v S v S v S v S v S v S v S v S v S v S v S v S V N
+        M h x h x h x h x h x h x h x h x h x h x h x h x h x h x h m N
+        V S v S v S v S v S v S v S v S v S v S v S v S v S v S v S V N
+        M h x h x h x h x h x h x h x h x h x h x h x h x h x h x h m N
+        V S v S v S v S v S v S v S v S v S v S v S v S v S v S v S V N
+        M h x h x h x h x h x h x h x h x h x h x h x h x h x h x h m N
+        V S v S v S v S v S v S v S v S v S v S v S v S v S v S v S V N
+        M h x h x h x h x h x h x h x h x h x h x h x h x h x h x h m N
+        V S v S v S v S v S v S v S v S v S v S v S v S v S v S v S V N
+        M h x h x h x h x h x h x h x h x h x h x h x h x h x h x h m N
+        V S v S v S v S v S v S v S v S v S v S v S v S v S v S v S V N
+        l H c H c H c H c H c H c H c H c H c H c H c H c H c H c H r N
         L'\0',
     };
     #undef N
@@ -49,22 +87,25 @@ int main(int argc, char const *argv[]) {
     #undef o
     #undef S
 
-    int count = 0;
-    for (int i = 0x2400; i < 0x2700; ++i) {
-        ++count;
-        wprintf(L"%lc, %X; ", i, i);
-        if ( 0 == ( count % 20 ) ) {
-            wprintf(L"\n");
-        }
-    }
-
+    FILE *outputFile = fopen("table.wchars", "w");
+    fwprintf(outputFile, table);
+    fclose(outputFile);
     wprintf(table);
+}
+
+int
+main(int argc, char const *argv[])
+{
+    wprintf(L"%s\n", setlocale(LC_ALL, ""));
+    wprintf(L">>>>>>> Belows are sandbox output:\n");
+    generateTableFile();
+
     system("stty raw");
     const wchar_t exit_flag = L'x';
     wchar_t input = 0;
     while(false == (exit_flag == input)) {
         input = getwchar();
-        wprintf(L"====>>> %X\n", input);
+        wprintf(L"====>>> %X\n\r", input);
     }
     system("stty cooked");
     return 0;
