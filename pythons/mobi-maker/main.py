226abd98e67bd1c16d692baf9dc6dc4e7cbdea93
XiaofengQi <qi_xiaofeng@foxmail.com>
2020-07-07 10:33:54 +0800

    Added template book.

---
+++ pythons/mobi-maker/main.py
@@ -0,0 +1,10 @@
+# 1. [WIP] Test kindlegen executable.
+#    - TOC.
+#    - Chapter.
+#    - Line-break.
+#    - /var/local/app-binaries/kindlegen-2.9/kindlegen
+# 2. Read file line by line.
+# 3. Parse line with regex.
+# 4. Prepare HTML template.
+# 5. Write HTML with parse result and template.
+# 6. Use kindlegen to make mobi file.
