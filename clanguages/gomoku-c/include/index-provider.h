19b2bc199a3ff90d3a5cccca911d264b768ef480
XiaofengQi <qi_xiaofeng@foxmail.com>
2020-07-13 09:25:26 +0800

    Prepared for extracting index provider out of test/main.

---
+++ clanguages/gomoku-c/include/index-provider.h
@@ -0,0 +1,8 @@
+//
+// Created by qixiaofeng on 2020/7/13.
+//
+
+#ifndef GOMOKU_C_INDEX_PROVIDER_H
+#define GOMOKU_C_INDEX_PROVIDER_H
+
+#endif //GOMOKU_C_INDEX_PROVIDER_H
---
+++ clanguages/gomoku-c/src/index-provider.c
@@ -0,0 +1,5 @@
+//
+// Created by qixiaofeng on 2020/7/13.
+//
+
+#include "index-provider.h"
