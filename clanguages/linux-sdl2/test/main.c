1bdd6b2f0fd8ab119d90975b755193a97f9af1f5
XiaofengQi <qi_xiaofeng@foxmail.com>
2020-08-21 11:48:42 +0800

    Added unit test for overlap checking.

--- clanguages/linux-sdl2/test/main.c
+++ clanguages/linux-sdl2/test/main.c
@@ -1,19 +1,25 @@
 #include "testool.h"
 #include "test-physics.h"
 
-void p_test_placeholder() {
+void p_test_is_overlap() {
     AABB aabbOne = {{0,  0,},
                     {10, 10,},};
-    AABB aabbTwo = {{10, 10,},
+    AABB aabbTwo = {{10.01, 10.01,},
                     {20, 20},};
+    p_print_aabb(&aabbOne, "test aabb");
     M_test_int(p_is_overlap(&aabbOne, &aabbTwo), false);
-    aabbTwo.leftTop.x = 9;
+    aabbTwo.leftTop.x = 10;
     M_test_int(p_is_overlap(&aabbOne, &aabbTwo), false);
-    aabbTwo.leftTop.y = 9;
+    aabbTwo.leftTop.y = 10;
+    M_test_int(p_is_overlap(&aabbOne, &aabbTwo), true);
+    aabbTwo.leftTop.y = 9.99;
+    M_test_int(p_is_overlap(&aabbOne, &aabbTwo), true);
+    aabbTwo.leftTop.x = -0.01;
+    aabbTwo.rightBottom.x = 10.01;
     M_test_int(p_is_overlap(&aabbOne, &aabbTwo), true);
 }
 
 int main() {
-    M_run_test_suite(p_test_placeholder);
+    M_run_test_suite(p_test_is_overlap);
     return 0;
 }
--- clanguages/linux-sdl2/test/test-physics.h
+++ clanguages/linux-sdl2/test/test-physics.h
@@ -6,6 +6,8 @@
 #define QXF_CANVAS_TEST_PHYCIS_H
 
 #include "physics.h"
+
 bool p_is_overlap(AABB *a, AABB *b);
+void p_print_aabb(AABB *aabb, char const *name);
 
 #endif //QXF_CANVAS_TEST_PHYCIS_H
