3527db38d8ab2291340c9af394d2bf9fdb94142d
XiaofengQi <qi_xiaofeng@foxmail.com>
2021-06-26 17:15:48 +0800

    Implemented newton fractal based on f = z^3 - 1.

--- clanguages/linux-sdl2-canvas/shaders/first.fs
+++ clanguages/linux-sdl2-canvas/shaders/first.fs
@@ -46,6 +46,58 @@ vec3 draw_parabola(vec2 pixel_position) {
   return vec3(1.0);
 }
 
+vec2 complex_multiply(vec2 a, vec2 b) {
+    return vec2(a.x * b.x - a.y * b.y, a.y * b.x + a.x * b.y);
+}
+
+vec2 complex_power(vec2 z, int p) {
+    vec2 result = z;
+    for (int i = 0; i < (p - 1); ++i) {
+        result = complex_multiply(result, z);
+    }
+    return result;
+}
+
+vec2 complex_divide(vec2 a, vec2 b) {
+    vec2 b2 = b * b;
+    float c = 1.0 / (b2.x + b2.y);
+    return vec2(a.x * b.x + a.y * b.y, a.y * b.x - a.x * b.y) * c;
+}
+
+vec2 f_for_newton_fractal(vec2 z) {
+  return complex_power(z, 3) - vec2(1, 0);
+}
+
+vec2 fd_for_newton_fractal(vec2 z) {
+  return 3.0 * complex_power(z, 2);
+}
+
+const vec2 roots[3] = vec2[3](
+  vec2(1, 0),
+  vec2(-0.5, 0.866),
+  vec2(-0.5, -0.866)
+);
+
+const vec3 colors[3] = vec3[3](
+  vec3(1.0, 0.8, 0.0),
+  vec3(0.0, 1.0, 0.8),
+  vec3(0.8, 0.0, 1.0)
+);
+
+vec3 draw_newton_fractal(vec2 pixel_position) {
+    vec2 z = pixel_position;
+    for (int i = 0; i < 100; ++i) {
+        z -= complex_divide(f_for_newton_fractal(z), fd_for_newton_fractal(z));
+        for (int j = 0; j < 3; ++j) {
+            vec2 d = z - roots[j];
+            if (length(d) < 1e-5) {
+                return colors[j];
+            }
+        }
+    }
+    return vec3(0.0);
+}
+
 // https://www.shadertoy.com/view/______
 void main() {
   // color = vec3(1.0, 0, mod(uniform_time, 1.0));
@@ -54,5 +106,6 @@ void main() {
   // color = draw_towers_7ljGzR(uv, vec3(0.0, 0.0, 0.0), uniform_time);
   // color = draw_shining_XsXXDn(uv, uniform_time);
   // color = draw_halo(uv, uniform_time);
-  color = draw_parabola(uv);
+  // color = draw_parabola(uv);
+  color = draw_newton_fractal(uv);
 }
