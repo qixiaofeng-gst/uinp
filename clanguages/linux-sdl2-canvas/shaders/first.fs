db86df6048552a572899407640d4e83d0c70ad2a
XiaofengQi <qi_xiaofeng@foxmail.com>
2021-06-28 09:56:30 +0800

    Separated shaders code.

--- clanguages/linux-sdl2-canvas/shaders/first.fs
+++ clanguages/linux-sdl2-canvas/shaders/first.fs
@@ -7,45 +7,6 @@ out vec3 color;
 uniform float uniform_time;
 uniform sampler2D texture_sampler;
 
-const float gray_scale = 1e-2;
-const float cycle_size = 1.0;
-const vec2 cycle_origin = vec2(0.5);
-
-vec3 draw_quater_circle(vec2 pixel_position) {
-  if (length(pixel_position) > 0.5) {
-    return vec3(pixel_position, 0.5);
-  } else {
-    return vec3(0.3, pixel_position * 1.5);
-  }
-}
-
-vec3 draw_towers_7ljGzR(vec2 pixel_position, vec3 old_color, float time) {
-  ivec3 b = ivec3(127, 128, 255);
-  for(;(b.x ^ b.y & b.z) % 200 > (b.z - 9);) {
-    b = ivec3((pixel_position / 5e2 - 0.5) * old_color.x + time /.1, old_color += .1);
-  }
-  return vec3(b * b.x % 2) + old_color / 2e2;
-}
-
-vec3 draw_shining_XsXXDn(vec2 pixel_position, float time) {
-  float x = mod(time, cycle_size) - cycle_size * 0.5;
-  float y = x * x;
-  return vec3(gray_scale) * y / length(pixel_position - cycle_origin);
-}
-
-vec3 draw_halo(vec2 pixel_position, float time) {
-  float x = mod(time, cycle_size) - cycle_size * 0.5;
-  float y = x * x;
-  return vec3(sin(length(pixel_position - cycle_origin) * 1e2 * y));
-}
-
-vec3 draw_parabola(vec2 pixel_position) {
-  if (abs((pixel_position.x * pixel_position.x) - pixel_position.y) < 1e-2) {
-    return vec3(0.0);
-  }
-  return vec3(1.0);
-}
-
 vec2 complex_multiply(vec2 a, vec2 b) {
     return vec2(a.x * b.x - a.y * b.y, a.y * b.x + a.x * b.y);
 }
@@ -85,8 +46,8 @@ const vec3 colors[3] = vec3[3](
 );
 
 vec3 draw_newton_fractal(vec2 pixel_position) {
-    vec2 z = pixel_position;
-    for (int i = 0; i < 100; ++i) {
+    vec2 z = pixel_position * 3.5 - 1.0;
+    for (int i = 0; i < 1024; ++i) {
         z -= complex_divide(f_for_newton_fractal(z), fd_for_newton_fractal(z));
         for (int j = 0; j < 3; ++j) {
             vec2 d = z - roots[j];
@@ -98,14 +59,6 @@ vec3 draw_newton_fractal(vec2 pixel_position) {
     return vec3(0.0);
 }
 
-// https://www.shadertoy.com/view/______
 void main() {
-  // color = vec3(1.0, 0, mod(uniform_time, 1.0));
-  // color = texture(texture_sampler, uv).rgb;
-  // color = draw_quater_circle(uv);
-  // color = draw_towers_7ljGzR(uv, vec3(0.0, 0.0, 0.0), uniform_time);
-  // color = draw_shining_XsXXDn(uv, uniform_time);
-  // color = draw_halo(uv, uniform_time);
-  // color = draw_parabola(uv);
   color = draw_newton_fractal(uv);
 }
---
+++ clanguages/linux-sdl2-canvas/shaders/shader_toy.fs
@@ -0,0 +1,50 @@
+#version 460 core
+
+in vec2 uv;
+
+out vec3 color;
+
+uniform float uniform_time;
+uniform sampler2D texture_sampler;
+
+const float gray_scale = 1e-2;
+const float cycle_size = 1.0;
+const vec2 cycle_origin = vec2(0.5);
+
+vec3 draw_quater_circle(vec2 pixel_position) {
+  if (length(pixel_position) > 0.5) {
+    return vec3(pixel_position, 0.5);
+  } else {
+    return vec3(0.3, pixel_position * 1.5);
+  }
+}
+
+vec3 draw_towers_7ljGzR(vec2 pixel_position, vec3 old_color, float time) {
+  ivec3 b = ivec3(127, 128, 255);
+  for(;(b.x ^ b.y & b.z) % 200 > (b.z - 9);) {
+    b = ivec3((pixel_position / 5e2 - 0.5) * old_color.x + time /.1, old_color += .1);
+  }
+  return vec3(b * b.x % 2) + old_color / 2e2;
+}
+
+vec3 draw_shining_XsXXDn(vec2 pixel_position, float time) {
+  float x = mod(time, cycle_size) - cycle_size * 0.5;
+  float y = x * x;
+  return vec3(gray_scale) * y / length(pixel_position - cycle_origin);
+}
+
+vec3 draw_halo(vec2 pixel_position, float time) {
+  float x = mod(time, cycle_size) - cycle_size * 0.5;
+  float y = x * x;
+  return vec3(sin(length(pixel_position - cycle_origin) * 1e2 * y));
+}
+
+// https://www.shadertoy.com/view/______
+void main() {
+  // color = vec3(1.0, 0, mod(uniform_time, 1.0));
+  // color = texture(texture_sampler, uv).rgb;
+  // color = draw_quater_circle(uv);
+  // color = draw_towers_7ljGzR(uv, vec3(0.0, 0.0, 0.0), uniform_time);
+  // color = draw_shining_XsXXDn(uv, uniform_time);
+  // color = draw_halo(uv, uniform_time);
+}
