0430792b126e2783d2cbb92ff710e2a52ee24c05
XiaofengQi <qi_xiaofeng@foxmail.com>
2021-07-02 14:07:47 +0800

    Added key information for trojan helper.

--- pythons/trojan_helper.py
+++ pythons/trojan_helper.py
@@ -1,24 +1,29 @@
 
 
 def __load_template():
+    # trojan_config_template.json
     print(__file__)
     pass
 
 
 def __load_nodes_list():
+    # surge_trojan_nodes.txt
     pass
 
 
 def __update_etc():
+    # password: list, remote_port: int, remote_addr: string.
+    # /etc/trojan/config.json
+    pass
+
+
+def __restart_trojan():
+    # sudo systemctl restart trojan
     pass
 
 
 def __test_selection():
     # curl -v --socks5 127.0.0.1:1080 --connect-timeout 3 http://www.youtube.com
-    pass
-
-
-def __restart_trojan():
     pass
 
 
