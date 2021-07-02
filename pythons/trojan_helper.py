092b09c3aeeaf1729dc399e110d2f15e93a432d8
XiaofengQi <qi_xiaofeng@foxmail.com>
2021-07-02 18:26:18 +0800

    Implemented ability to update the trojan configuration.

--- pythons/trojan_helper.py
+++ pythons/trojan_helper.py
@@ -1,9 +1,16 @@
+import json as __json
+import os as __os
+import configparser as __cp
+
+
+def __get_file(file_name):
+    return __os.path.join(__os.path.dirname(__file__), file_name)
 
 
 def __load_template():
-    # trojan_config_template.json
-    print(__file__)
-    pass
+    template_path = __get_file('trojan_config_template.json')
+    with open(template_path, 'r') as file:
+        return __json.load(file)
 
 
 def __load_nodes_list():
@@ -11,15 +18,20 @@ def __load_nodes_list():
     pass
 
 
-def __update_etc():
-    # password: list, remote_port: int, remote_addr: string.
-    # /etc/trojan/config.json
-    pass
+def __update_etc(config_object: dict, remote_addr: str, remote_port: int, password: str):
+    config_object.update(
+        remote_addr=remote_addr,
+        remote_port=remote_port,
+        password=[password],
+    )
+    to_update = '/etc/trojan/config.json'
+    with open(to_update, 'w') as file:
+        __json.dump(config_object, file)
+    print('Updated:', to_update)
 
 
 def __restart_trojan():
-    # sudo systemctl restart trojan
-    pass
+    print('Tried restarting trojan:', __os.system('systemctl restart trojan'))
 
 
 def __test_selection():
@@ -28,7 +40,10 @@ def __test_selection():
 
 
 def __main():
-    print('Hello world')
+    template_object = __load_template()
+    __update_etc(template_object, 'jpa1.v2dot.online', 443, '53677FFF-F237-93D8-15A3-0991112C4233')
+    # __restart_trojan()
+    print('Use `journalctl -u trojan -b -f` to follow the log of trojan.')
 
 
 if __name__ == '__main__':
