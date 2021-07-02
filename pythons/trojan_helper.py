c16ee247e94391c1cc29bb8c85fb441caa36a587
XiaofengQi <qi_xiaofeng@foxmail.com>
2021-07-02 14:00:33 +0800

    Prepared workflow for trojan helper.

--- pythons/trojan_helper.py
+++ pythons/trojan_helper.py
@@ -1,4 +1,30 @@
-# curl -v --socks5 127.0.0.1:1080 http://www.youtube.com
+
+
+def __load_template():
+    print(__file__)
+    pass
+
+
+def __load_nodes_list():
+    pass
+
+
+def __update_etc():
+    pass
+
+
+def __test_selection():
+    # curl -v --socks5 127.0.0.1:1080 --connect-timeout 3 http://www.youtube.com
+    pass
+
+
+def __restart_trojan():
+    pass
+
+
+def __main():
+    print('Hello world')
+
 
 if __name__ == '__main__':
-    print('Hello world')
+    __main()
