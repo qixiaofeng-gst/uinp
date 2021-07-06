a0d890f7d7d3dd3a9fc5d00277308f28b462c3b0
XiaofengQi <qi_xiaofeng@foxmail.com>
2021-07-06 09:53:49 +0800

    Removed the locating code.

--- clanguages/linux-sdl2-canvas/shaders/mandelbrot.fs
+++ clanguages/linux-sdl2-canvas/shaders/mandelbrot.fs
@@ -15,13 +15,9 @@ Play with Mandelbrot set: z_{n+1} = z_n^2 + c, z_0 = 0 + 0i
 The 'c' is the given pixel position, render the iteration count that make z_n > 2
 */
 
+/** TODO: Give a mouse position as scalation center. */
 vec3 draw_mandelbrot(vec2 point, float passed_time) {
-    // vec2 c = point / passed_time - vec2(0.1, 0.25);
     vec2 c = point / (passed_time * 1e3) + vec2(0.3151, 0.5589);
-    vec2 p = mod(c, 0.25);
-    if (length(p) < 0.01) {
-        return vec3(1.0);
-    }
     vec2 z = vec2(0);
     float max_iteration = 1024.0;
     for (int i = 0; i < max_iteration; ++i) {
