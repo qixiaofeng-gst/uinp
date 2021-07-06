74bf091e9d0be7bd555be4a1caa254d95c1865b7
XiaofengQi <qi_xiaofeng@foxmail.com>
2021-07-06 09:49:04 +0800

    Created a scalable mandelbrot fractal.

--- clanguages/linux-sdl2-canvas/shaders/mandelbrot.fs
+++ clanguages/linux-sdl2-canvas/shaders/mandelbrot.fs
@@ -16,7 +16,12 @@ The 'c' is the given pixel position, render the iteration count that make z_n > 
 */
 
 vec3 draw_mandelbrot(vec2 point, float passed_time) {
-    vec2 c = point / passed_time - vec2(0.1, 0.25);
+    // vec2 c = point / passed_time - vec2(0.1, 0.25);
+    vec2 c = point / (passed_time * 1e3) + vec2(0.3151, 0.5589);
+    vec2 p = mod(c, 0.25);
+    if (length(p) < 0.01) {
+        return vec3(1.0);
+    }
     vec2 z = vec2(0);
     float max_iteration = 1024.0;
     for (int i = 0; i < max_iteration; ++i) {
